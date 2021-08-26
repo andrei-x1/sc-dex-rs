7bd5e58db8c9c9150eb6338c2051da1701514a2b
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-08-26 14:44:40 +0300

    added funcs to send multiple tokens

--- common/modules/token_send/src/token_send.rs
+++ common/modules/token_send/src/token_send.rs
@@ -65,6 +65,79 @@ pub trait TokenSendModule {
         ))
     }
 
+    fn send_multiple_tokens(
+        &self,
+        payments: &[EsdtTokenPayment<Self::TypeManager>],
+        destination: &Address,
+        opt_accept_funds_func: &OptionalArg<BoxedBytes>,
+    ) -> SCResult<()> {
+        let (function, gas_limit) = match opt_accept_funds_func {
+            OptionalArg::Some(accept_funds_func) => (
+                accept_funds_func.as_slice(),
+                self.transfer_exec_gas_limit().get(),
+            ),
+            OptionalArg::None => {
+                let no_func: &[u8] = &[];
+                (no_func, 0u64)
+            }
+        };
+
+        SCResult::from_result(self.send().direct_multi_esdt_transfer_execute(
+            destination,
+            payments,
+            gas_limit,
+            function,
+            &ArgBuffer::new(),
+        ))
+    }
+
+    fn send_multiple_tokens_compact(
+        &self,
+        payments: &[EsdtTokenPayment<Self::TypeManager>],
+        destination: &Address,
+        opt_accept_funds_func: &OptionalArg<BoxedBytes>,
+    ) -> SCResult<()> {
+        let mut compact_payments = Vec::<EsdtTokenPayment<Self::TypeManager>>::new();
+        for payment in payments.iter() {
+            if payment.amount != 0 {
+                let existing_index = compact_payments.iter().position(|x| {
+                    x.token_name == payment.token_name && x.token_nonce == payment.token_nonce
+                });
+
+                match existing_index {
+                    Some(index) => compact_payments[index].amount += &payment.amount,
+                    None => compact_payments.push(payment.clone()),
+                }
+            }
+        }
+
+        let len = compact_payments.len();
+        if len == 1 {
+            let payment = &compact_payments[0];
+
+            if payment.token_nonce == 0 {
+                self.send_fft_tokens(
+                    &payment.token_name,
+                    &payment.amount,
+                    destination,
+                    opt_accept_funds_func,
+                )
+            } else {
+                self.send_nft_tokens(
+                    &payment.token_name,
+                    payment.token_nonce,
+                    &payment.amount,
+                    destination,
+                    opt_accept_funds_func,
+                )
+            }
+        } else if len > 1 {
+            self.send_multiple_tokens(&compact_payments, destination, opt_accept_funds_func)
+        } else {
+            Ok(())
+        }
+    }
+
     #[view(getTransferExecGasLimit)]
     #[storage_mapper("transfer_exec_gas_limit")]
     fn transfer_exec_gas_limit(&self) -> SingleValueMapper<Self::Storage, u64>;
